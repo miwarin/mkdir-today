f31b49f58ce577e57197919eac6591770aa01995
Susumu Miwa <miwarin@gmail.com>
2014-05-13 20:15:15 +0900

    今日の日付のフォルダを作成する

---
+++ mkdir-today/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace mkdir_today
+{
+    class Program
+    {
+        static private String Today()
+        {
+            DateTime d = DateTime.Today;
+            String today = d.ToString("yyyy/MM/dd");
+            return today;
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("コマンドライン引数はありません。");
+            }
+            else
+            {
+                String basedir = Path.GetDirectoryName(args[0]);
+                if (Directory.Exists(basedir) == false)
+                {
+                    Console.WriteLine("ディレクトリを指定してね");
+                    return;
+                }
+
+                String today = Today();
+                String path = Path.Combine(basedir, today);
+                Console.WriteLine("create {0}", path);
+                Directory.CreateDirectory(path);
+            }
+        }
+    }
+}
